d638be2daf8939a0e30dbe5101ea37081a3615cf
Emmanuelk2001 <sge.kouame.25@eigsica.ma>
2023-01-22 15:07:10 +0100

    first step of buiding a blog web site

--- index.php
+++ index.php
@@ -1,1 +1,53 @@
-<?php
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>my.blog</title>
+      <link rel="stylesheet" href="style.css">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+        <header>
+            <nav>
+                <h1><span>My</span><span>.</span>blog</h1>
+                <div class ="right">
+                     <a href="#"><p>blog</p></a>
+                     <a href="#"><p>contacts</p></a>
+                     <a href="#"><p>about us</p></a>
+                     <a href="https://www.16personalities.com/free-personality-test"><button> sign in </button></a>
+                        <a href="https://www.16personalities.com/free-personality-test"><button> sign up</button></a>
+                    
+                </div>
+                    
+            </nav>
+            <section class="presentation">
+                <div class="abstract-presentation">
+                    <h1>buid your blog in <br> few minutes</h1>
+                     <p>
+                    join the community shay your stories on your blog.
+                    </p>
+                </div>
+            </section>
+        </header>
+        <footer >
+                <div class="container">
+                    <h3>Contactez moi</h3>
+                    <p>keelfsfejfe,ffedf,mf,e,fm,rr,ef,kefkdf,dmf,, djejjnf jdefnknc kndfe<br/>
+                        dfnjekknfndnfehhenf hjffhr hfnzrvkehrf jzfhekfnlkfen fnkffzls
+                    </p>
+                    <ul class="socials">
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                        <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li>
+                    </ul>
+                   
+                </div>
+            </footer>
+    </body>
+    
+</html>
